02d70e611e4e4d37bffb630672344ac23a61aaab
Cezstro <raheem.hardy@gmail.com>
2017-08-09 17:50:13 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,15 +1,26 @@
 
+$( document ).ready(function() {
+    console.log( "ready!" );
 
-//Use key events to listen for the letters that your players will type.
-
-
-//Display the following on the page: Press any key to get started!
-
-
-//
+    //Press any key to get started
 
 
-//
+    //wins var
 
 
-//
+    //display word as underscores (for each letter in word...)
+
+
+    //number of guesses remaining
+
+
+    //letters guessed
+
+    //After the user wins/loses the game should automatically choose another word and make the user play it.
+
+    //play song on win
+
+    //play song on lose
+
+    //organize into an object
+});

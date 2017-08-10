2e6236b0f11f55b5ef2b7044480ff030b6a3e56b
Cezstro <raheem.hardy@gmail.com>
2017-08-10 16:31:41 -0400

    most recent changes to js

--- main.js
+++ main.js
@@ -1,9 +1,50 @@
 
 $( document ).ready(function() {
-    console.log( "ready!" );
 
+
+    // need to PUT THIS SOMEWHERE ELSE..?
+    $("#hangman-board").hide();
     //Press any key to get started
+    var btn_start = document.getElementById("button-start");
+    var hangman_box = document.getElementById("hangman-box");
 
+    reset_game();
+
+    $(document).keypress( function(event){
+        new_game();    
+        console.log('Handler for .keypress function called. = ' + event.charCode);    
+    } );
+
+    // new game 
+    function new_game(){
+        console.log("new game");
+
+        //start new hangman board for word
+        hangman_board_init();
+        //stop tracking keypress function
+        $(document).keydown(function(){
+            console.log("keypress function is stopped...");
+            return false; 
+        });
+    }
+
+    function reset_game(){
+        $("#hangman-body").hide();
+    }
+
+    //show the hangman board, reset letters left
+    function hangman_board_init(){
+        var hangman_word = document.getElementById("hangman-board");
+        var hangman_text = document.getElementById("get-started");
+        var letters_left = document.getElementById("letters-left");
+
+        $("#hangman-board").show();
+        $(hangman_text).html("CURRENT WORD!");
+        $(letters_left).html("0");
+        $(hangman_word).html("IRON MAN")
+
+        console.log("BOARD IS ACTIVE");
+    }
 
     //wins var
 
